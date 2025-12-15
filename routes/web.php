973bfded73170c91e93fba13de352ee002e0fb74
rianlesmanaputra <bangrian2018@gmail.com>
2025-12-15 14:08:12 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
 use App\Http\Controllers\User\BerandaController;
+use App\Http\Controllers\User\BeritaController;
 
 Route::get('/', [BerandaController::class, 'dashboard']);
+
+Route::get('/berita', [BeritaController::class, 'index'])->name('berita.index');
+Route::get('/berita/{slug}', [BeritaController::class, 'show'])->name('berita.show');
