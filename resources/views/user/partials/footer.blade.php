a92e1f3a3dded7e05716916b7aed23f00910d077
Afif22ti <afif22ti@mahasiswa.pcr.ac.id>
2025-12-23 15:22:19 +0700

    Add favicon to layouts

--- resources/views/user/partials/footer.blade.php
+++ resources/views/user/partials/footer.blade.php
@@ -1,55 +1,23 @@
-<footer class="footer-riau">
-
-    <!-- FOOTER ATAS -->
+<footer class="footer-riau"> <!-- FOOTER ATAS -->
     <div class="footer-top">
         <div class="container footer-top-wrap">
-
             <div class="footer-info">
                 <h4>Ingin mengetahui lebih lanjut?</h4>
-                <p>
-                    Kunjungi media resmi Balai Bahasa Provinsi Riau
-                    untuk memperoleh informasi kebahasaan dan kesastraan.
-                </p>
+                <p> Kunjungi media resmi Balai Bahasa Provinsi Riau untuk memperoleh informasi kebahasaan dan kesastraan. </p>
             </div>
-
         </div>
-    </div>
-
-    <!-- GARIS PEMBATAS -->
-    <div class="footer-divider"></div>
-
-    <!-- FOOTER TENGAH -->
+    </div> <!-- GARIS PEMBATAS -->
+    <div class="footer-divider"></div> <!-- FOOTER TENGAH -->
     <div class="footer-main">
-        <div class="container footer-main-wrap">
-
-            <!-- ALAMAT -->
-            <div class="footer-col">
-                <img src="/img/logo.png" alt="BBP Riau" class="footer-logo">
-
-                <p>
-                    Jalan H.R. Soebrantas<br>
-                    Km. 12,5 Kampus Binawidya UNRI<br>
-                    Simpang Baru, Tampan,<br>
-                    Kota Pekanbaru, Riau.
-                </p>
-
-                <p>
-                    Telepon: (0761) 65930<br>
-                    Faksimile: (0761) 65930
-                </p>
-            </div>
-
-            <!-- PETA -->
+        <div class="container footer-main-wrap"> <!-- ALAMAT -->
+            <div class="footer-col"> <img src="/img/logo.png" alt="BBP Riau" class="footer-logo">
+                <p> Jalan H.R. Soebrantas<br> Km. 12,5 Kampus Binawidya UNRI<br> Simpang Baru, Tampan,<br> Kota Pekanbaru, Riau. </p>
+                <p> Telepon: (0761) 65930<br> Faksimile: (0761) 65930 </p>
+            </div> <!-- PETA -->
             <div class="footer-col">
                 <h4>Peta</h4>
-                <div class="footer-map">
-                    <iframe src="https://www.google.com/maps?q=Balai+Bahasa+Provinsi+Riau&output=embed" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade">
-                    </iframe>
-                </div>
-            </div>
-
-            <!-- TENTANG KAMI -->
+                <div class="footer-map"> <iframe src="https://www.google.com/maps?q=Balai+Bahasa+Provinsi+Riau&output=embed" loading="lazy" referrerpolicy="no-referrer-when-downgrade"> </iframe> </div>
+            </div> <!-- TENTANG KAMI -->
             <div class="footer-col">
                 <h4>Tentang Kami</h4>
                 <ul>
@@ -60,13 +28,9 @@
                     <li><a href="#">Risalah Kebijakan</a></li>
                 </ul>
             </div>
-
         </div>
-    </div>
-
-    <!-- FOOTER BAWAH -->
+    </div> <!-- FOOTER BAWAH -->
     <div class="footer-bottom">
         <p>© 2025 Balai Bahasa Provinsi Riau</p>
     </div>
-
 </footer>
