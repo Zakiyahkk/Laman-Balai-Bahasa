798ae433815a46210cf6d6fb581b492296e11e07
Afif22ti <afif22ti@mahasiswa.pcr.ac.id>
2026-01-23 09:23:17 +0700

    update 2

--- resources/views/user/beranda/slider.blade.php
+++ resources/views/user/beranda/slider.blade.php
@@ -58,8 +58,5 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
     <script src="{{ asset('js/slider.js') }}"></script>
-
 </body>
-@include('user.beranda.poppantun')
-
 </html>
