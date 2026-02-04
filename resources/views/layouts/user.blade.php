9d7c73e64c09281968e7dc45bf6975a4d881017e
Leebsite <mhd.ghalibpradipa@gmail.com>
2026-02-04 10:40:25 +0700

    march cpanel in views/layouts -> user.blade

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -6,43 +6,54 @@
     <title>@yield('title') | Balai Bahasa</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{-- FONT AWESOME (ICON RESMI) --}}
+    {{-- BOOTSTRAP 5 (WAJIB) --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    {{-- FONT AWESOME --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    {{-- GOOGLE FONT --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap"
         rel="stylesheet">
-    {{-- ICONS/LOGO --}}
+
+    {{-- ICON --}}
     <link rel="icon" type="image/png" href="{{ asset('img/gambar1.png') }}">
     <link rel="apple-touch-icon" href="{{ asset('img/gambar1.png') }}">
 
-    {{-- CSS USER --}}
+    {{-- CSS GLOBAL USER --}}
     <link rel="stylesheet" href="{{ asset('css/user.css') }}?v={{ time() }}">
 
-    <link rel="stylesheet" href="{{ asset('css/ziwbk.css') }}">
+    {{-- CSS ZI-WBK --}}
+    <link rel="stylesheet" href="{{ asset('css/ziwbk.css') }}?v={{ time() }}">
 
-    <!-- CSS Halaman Spesifik -->
+    {{-- CSS PER HALAMAN --}}
     @yield('css')
 </head>
 
 <body>
 
-    {{-- HEADER LENGKAP (TOPBAR + LOGO + NAVBAR) --}}
+    {{-- HEADER --}}
     @include('user.partials.header')
 
-    <main>
+    <main class="py-4">
         @yield('content')
     </main>
 
     {{-- FOOTER --}}
     @include('user.partials.footer')
 
-    {{-- CHATBOT FLOATING --}}
+    {{-- CHATBOT --}}
     @include('user.beranda.chatbot')
+
+    {{-- JS BOOTSTRAP --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
     {{-- JS USER --}}
     <script src="{{ asset('js/user.js') }}"></script>
-    {{-- JS HALAMAN (PREVIEW, DLL) --}}
+
+    {{-- JS PER HALAMAN --}}
     @stack('scripts')
     @yield('scripts')
 
